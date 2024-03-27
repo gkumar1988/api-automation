a6dd8ccb6c1766c1d532f32050eae80e3feeba2d
gkumar1988 <66743365+gkumar1988@users.noreply.github.com>
2024-03-27 11:43:16 -0400

    first-commit

---
+++ src/test/java/com/example/apiautomation/ApiAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
